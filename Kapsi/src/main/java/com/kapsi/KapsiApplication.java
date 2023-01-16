fea086074695033495da9499b7ed5bf3f5ad0be1
Coder Abhi <coder.abhi0713@gmail.com>
2023-01-16 19:53:42 +0530

    Kapsi Project Application added

---
+++ Kapsi/src/main/java/com/kapsi/KapsiApplication.java
@@ -0,0 +1,13 @@
+package com.kapsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapsiApplication.class, args);
+	}
+
+}
---
+++ Kapsi/src/test/java/com/kapsi/KapsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
