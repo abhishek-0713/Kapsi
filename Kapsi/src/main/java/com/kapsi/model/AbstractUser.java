24a59b6b858f9c031f46a2b684f72f94ea315f0d
jainendra singh <jainendrasingh398@gmail.com>
2023-01-22 15:19:57 +0530

    complete cab & driver

--- Kapsi/src/main/java/com/kapsi/model/AbstractUser.java
+++ Kapsi/src/main/java/com/kapsi/model/AbstractUser.java
@@ -1,12 +1,16 @@
 package com.kapsi.model;
 
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Data
@@ -16,12 +20,22 @@ public abstract class AbstractUser {
 
     @Column(unique = true)
     private String userName;
+  
+    @NotNull
+	@Size(min = 8,message="password should be minimum 8 characters")
     private String password;
+    
+    @NotNull
     private String address;
 
-    @Column(unique = true)
+    
+  	@NotNull
+  	@Column(unique = true)
+  	@Pattern(regexp = "[6789]{1}[0-9]{9}", message = "Enter valid 10 digit mobile number")
     private String mobileNumber;
-    private String email;
 
+	@NotNull
+	@Email
+    private String email;
 
 }
